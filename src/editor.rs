e2180d92eceaed3d16aa5fcb84ebaed6e3c1244f
Harsh Shandilya <me@msfjarvis.dev>
2022-08-07 17:47:17 +0530

    feat(editor): print a goodbyte message and clear screen before exiting

--- src/editor.rs
+++ src/editor.rs
@@ -34,6 +34,9 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), io::Error> {
         print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Goodbye.\r");
+        }
         io::stdout().flush()
     }
 }
@@ -47,5 +50,6 @@ fn read_key() -> Result<Key, io::Error> {
 }
 
 fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }

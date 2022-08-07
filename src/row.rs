d7ae3fdde079c27d2a7b78f5105069e6a110603a
Harsh Shandilya <me@msfjarvis.dev>
2022-08-08 00:21:24 +0530

    fix(row): convert tabs in document to spaces during rendering

--- src/row.rs
+++ src/row.rs
@@ -27,7 +27,11 @@ impl Row {
             .skip(start)
             .take(end - start)
         {
-            result.push_str(grapheme)
+            if grapheme == "\t" {
+                result.push(' ');
+            } else {
+                result.push_str(grapheme);
+            }
         }
         result
     }

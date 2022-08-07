c566116b9767e0b9864d90c7040903aba818e6bc
Harsh Shandilya <me@msfjarvis.dev>
2022-08-08 00:00:20 +0530

    feat(row): make line length Unicode-aware

--- src/row.rs
+++ src/row.rs
@@ -1,14 +1,19 @@
 use std::cmp;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub struct Row {
     contents: String,
+    len: usize,
 }
 
 impl From<&str> for Row {
     fn from(slice: &str) -> Self {
-        Self {
+        let mut row = Self {
             contents: String::from(slice),
-        }
+            len: 0,
+        };
+        row.update_len();
+        row
     }
 }
 
@@ -16,17 +21,26 @@ impl Row {
     pub fn render(&self, start: usize, end: usize) -> String {
         let end = cmp::min(end, self.contents.len());
         let start = cmp::min(start, end);
-        self.contents
-            .get(start..end)
-            .unwrap_or_default()
-            .to_string()
+        let mut result = String::new();
+        for grapheme in self.contents[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            result.push_str(grapheme)
+        }
+        result
     }
 
     pub fn len(&self) -> usize {
-        self.contents.len()
+        self.len
     }
 
     pub fn is_empty(&self) -> bool {
-        self.contents.is_empty()
+        self.len == 0
+    }
+
+    pub fn update_len(&mut self) {
+        self.len = self.contents[..].graphemes(true).count();
     }
 }

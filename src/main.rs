5f7572fd558e09383c13c80e1a7e694eed5fefc7
Harsh Shandilya <me@msfjarvis.dev>
2022-08-07 22:55:51 +0530

    feat: add types for document rendering

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod document;
 mod editor;
+mod row;
 mod terminal;
 
+pub use crate::document::Document;
 use crate::editor::Editor;
 pub use crate::editor::Position;
+pub use crate::row::Row;
 pub use crate::terminal::Terminal;
 use anyhow::Result;
 

5413e7c9037e0bc70a1a2eba6eac5a4a2a18b20e
Harsh Shandilya <me@msfjarvis.dev>
2022-08-07 17:21:46 +0530

    refactor(editor): separate reading and evaluating keypresses

--- src/editor.rs
+++ src/editor.rs
@@ -8,22 +8,27 @@ pub struct Editor {}
 impl Editor {
     pub fn run(&self) -> Result<()> {
         let _stdout = stdout().into_raw_mode()?;
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?} \r", c as u8);
-                        } else {
-                            println!("{:?} ({}) \r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?} \r", key),
-                },
-                Err(e) => bail!(e),
+        loop {
+            if let Err(error) = self.process_keypress() {
+                bail!(error);
             }
         }
+    }
+
+    fn process_keypress(&self) -> Result<()> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => bail!("Program end"),
+            _ => (),
+        };
         Ok(())
+    }
+}
+
+fn read_key() -> std::result::Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
     }
 }

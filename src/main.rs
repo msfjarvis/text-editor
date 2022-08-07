88f8a2b1c3eed029624ad09fc393936c37e1e8ce
Harsh Shandilya <me@msfjarvis.dev>
2022-08-07 16:34:14 +0530

    feat: check for control characters and print more info

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use anyhow::Result;
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() -> Result<()> {
+    let _stdout = stdout().into_raw_mode()?;
     for b in io::stdin().bytes() {
-        let c = b? as char;
+        let b = b?;
+        let c = b as char;
+        if c.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({}) \r", b, c);
+        }
         if c == 'q' {
             break;
         }
